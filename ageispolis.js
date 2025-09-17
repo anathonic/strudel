c6d6c20493ce860b2c8361891a76951f199faf8d
Anath <anathonic@gmail.com>
2025-09-18 00:36:16 +0200

    Update ageispolis.js

--- ageispolis.js
+++ ageispolis.js
@@ -4,29 +4,20 @@
 setcpm(102/4)
 
 const synth = note(`<
-[c6 ~ a#5 ~ g#5 ~ a#5 c6 ~ f5 ~ f5 ~ f5 g#5 a#5 ~ a#5 g#5 ~ a#5 ~ g#5 ~ a#5 g#5 f5 d#5 ~ f5 d#5 f5 ~ ]
+[c6 ~ a#5 ~ g#5 ~ a#5 c6 ~ f5 ~ f5 ~ f5 g#5 a#5 ~ a#5 g#5 ~ a#5 ~ g#5 ~ a#5 g#5 f5 d#5 ~ f5 d#5 f5 ~ ~ ]
 [f4 ~ g#4 a#4 c5 ~ c5 a#4 c5 ~ a#4 ~ c5 a#4 ~ c5 ~ ~ c5 ~ d#5 ~ f5 d#5 c5 ~ d#5 ~ c5 d5 ~ ~] 
 [f5 ~ f5 ~ g5 ~ g#5 f5 g#5 ~ a#5 ~ a#5 g#5 ~ f5 ~ ~ a#4 ~ c5 ~ d#5 ~ f5 ~ ~ ~ ~ ~ ]
-[f5 d#5 d#5 f5 ~ ~ ~ ~ c5 ~ c5 ~ d#5 ~ ~ ~ ~ f4 ~ g#4 ~ a#4 ~ c5 c4 c4 ~ c4 d#4 f4 ~]>*4`)
-  .sound("gm_synth_choir").slow(8).gain(0.9).room(2)
+[f5 d#5 d#5 f5 ~ ~ ~ ~ c5 ~ c5 ~ d#5 ~ ~ ~ ~ f4 ~ g#4 ~ a#4 ~ c5 c4 c4 ~ c4 d#4 f4 ~]
+>*4`).sound("gm_synth_choir").slow(8).gain(0.7).room(2)
 
-const drums1 = sound("bd ~ ~ bd ~ ~ bd ~ ~ ~ bd ~ ~ ~ ~").gain(0.5)
-.bank("RolandTR909")
+const drums = sound("bd ~ ~ bd ~ ~ bd ~ ~ ~ oh ~ hh ~ ~ ~ ~ ").gain(0.8)
+.bank("ace").room(0.2)
 
-const drums2 = sound(
-  "sn bd sn bd sn bd sn bd sn bd sn bd sn bd rd")
-.gain(0.2)
-.bank("RolandTR909")
-
-const drums3 = sound(
-  "~ ~ ~ ~ hh ~ ~ hh ~ ~ hh ~ ~ ~ hh")
-.gain(0.2)
-.bank("RolandTR909")
+const pad = note(`< [b3] ~ [d3] ~ >`).sound("gm_fx_soundtrack").duration(2).gain(0.8)
 
 const green = "#5FB147"
 stack(
-  arrange([15, drums1]),
+  arrange([16, drums]),
   arrange([16, synth]),
-  arrange([15, drums2]),
-  arrange([15, drums3]),
+  arrange([16,pad])
 ).color(green)._pianoroll()
